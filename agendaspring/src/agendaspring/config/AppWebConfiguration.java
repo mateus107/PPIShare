e5db7756802779ad061392146d0dccb147a5a606
20151074010107 <20151074010107@MC266459.ifrn.local>
2018-10-01 11:24:47 -0300

    configurando o viewResolver do Spring.  Agora o spring sabe onde as paginas estaram

--- agendaspring/src/agendaspring/config/AppWebConfiguration.java
+++ agendaspring/src/agendaspring/config/AppWebConfiguration.java
@@ -1,14 +1,20 @@
 package agendaspring.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import agendaspring.controllers.HomeController;
 
 @EnableWebMvc
 @ComponentScan (basePackageClasses= {HomeController.class})
 public class AppWebConfiguration {
-
-	
-	
+@Bean
+	public InternalResourceViewResolver internalResourceViewResolver() { 
+		InternalResourceViewResolver resolver = internalResourceViewResolver();
+		
+		resolver.setPrefix("/WEB-INF/views/");
+		return resolver;
+}
 }
